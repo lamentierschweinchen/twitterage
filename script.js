9811da1915b98e623ec742f624310ab591e6212c
lamentierschweinchen <64620972+lamentierschweinchen@users.noreply.github.com>
2023-12-30 12:32:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ function getBehaviorDescription(twitterAge) {
 
   const roundedAge = Math.round(twitterAge); // Round the calculated age
 
-  if (roundedAge <= 5) {
-    behavior = "You're throwing Twitter tantrums like a toddler denied their favorite toy!";
+  if (roundedAge <= 1) {
+    behavior = "You're tweeting like a crying baby, throwing tantrums with each tweet!";
+    imageSrc = 'images/crying_baby.jpg'; // Update path to crying baby image
+  } else if (roundedAge > 1 && roundedAge <= 3) {
+    behavior = "Your tweets resemble a toddler's playful, curious babble!";
     imageSrc = 'images/toddler.jpg'; // Update path to toddler image
-  } else if (roundedAge > 5 && roundedAge <= 10) {
-    behavior = "You're the petulant prince/princess of Twitter, always wanting attention and retweets!";
+  } else if (roundedAge > 3 && roundedAge <= 5) {
+    behavior = "Your Twitter presence is as chaotic and messy as a mischievous preschooler's playroom!";
+    imageSrc = 'images/mischievous_preschooler.jpg'; // Update path to mischievous preschooler image
+  } else if (roundedAge > 5 && roundedAge <= 7) {
+    behavior = "You're tweeting like a whiny grade-schooler, complaining about everything!";
     imageSrc = 'images/kid.jpg'; // Update path to kid image
+  } else if (roundedAge > 7 && roundedAge <= 10) {
+    behavior = "Your tweets are as irritating and rebellious as a stubborn pre-teen!";
+    imageSrc = 'images/stubborn_pre_teen.jpg'; // Update path to stubborn pre-teen image
   } else if (roundedAge > 10 && roundedAge <= 15) {
     behavior = "Your tweets are a whirlwind of teenage rebellion and immaturity!";
     imageSrc = 'images/teenager.jpg'; // Update path to teenager image
@@ -19,12 +28,9 @@ function getBehaviorDescription(twitterAge) {
   } else if (roundedAge > 18 && roundedAge <= 30) {
     behavior = "Your Twitter antics are the epitome of youthful exuberance and occasional immaturity!";
     imageSrc = 'images/mature.jpg'; // Update path to mature age image
-  } else if (roundedAge > 30 && roundedAge <= 50) {
-    behavior = "Your Twitter presence combines wisdom with occasional 'back in my day' rants!";
-    imageSrc = 'images/wise.jpg'; // Update path to wise age image
   } else {
     behavior = "You're the sage of Twitter, occasionally throwing shade like a well-aged fine wine.";
-    imageSrc = 'images/wise.jpg'; // Update path to wise age image (for default)
+    imageSrc = 'images/wise.jpg'; // Update path to wise age image
   }
 
   const result = {
