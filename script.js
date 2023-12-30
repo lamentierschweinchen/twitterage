727361ef46b35cf33e4589312a9546bef66221b6
lamentierschweinchen <64620972+lamentierschweinchen@users.noreply.github.com>
2023-12-30 02:48:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,67 @@
-// Function to calculate Twitter age
+function getBehaviorDescription(twitterAge) {
+  let behavior = '';
+  let imageSrc = '';
+
+  const roundedAge = Math.round(twitterAge); // Round the calculated age
+
+  if (roundedAge <= 5) {
+    behavior = "You're throwing Twitter tantrums like a toddler denied their favorite toy!";
+    imageSrc = 'images/toddler.jpg'; // Update path to toddler image
+  } else if (roundedAge > 5 && roundedAge <= 10) {
+    behavior = "You're the petulant prince/princess of Twitter, always wanting attention and retweets!";
+    imageSrc = 'images/kid.jpg'; // Update path to kid image
+  } else if (roundedAge > 10 && roundedAge <= 15) {
+    behavior = "Your tweets are a whirlwind of teenage rebellion and immaturity!";
+    imageSrc = 'images/teenager.jpg'; // Update path to teenager image
+  } else if (roundedAge > 15 && roundedAge <= 18) {
+    behavior = "You're a walking contradiction on Twitter - part adult, part teenager, and all drama!";
+    imageSrc = 'images/young.jpg'; // Update path to young age image
+  } else if (roundedAge > 18 && roundedAge <= 30) {
+    behavior = "Your Twitter antics are the epitome of youthful exuberance and occasional immaturity!";
+    imageSrc = 'images/mature.jpg'; // Update path to mature age image
+  } else if (roundedAge > 30 && roundedAge <= 50) {
+    behavior = "Your Twitter presence combines wisdom with occasional 'back in my day' rants!";
+    imageSrc = 'images/wise.jpg'; // Update path to wise age image
+  } else {
+    behavior = "You're the sage of Twitter, occasionally throwing shade like a well-aged fine wine.";
+    imageSrc = 'images/wise.jpg'; // Update path to wise age image (for default)
+  }
+
+  const result = {
+    behavior: behavior,
+    imageSrc: imageSrc
+  };
+
+  return result;
+}
+
 function calculateTwitterAge() {
   const actualAge = parseInt(document.getElementById('actualAge').value);
   const twitterYears = parseInt(document.getElementById('twitterYears').value);
 
   if (isNaN(actualAge) || isNaN(twitterYears) || twitterYears === 0) {
     document.getElementById('result').innerText = 'Please enter valid numbers.';
-    document.getElementById('ageImage').style.display = 'none';
     return;
   }
 
   const twitterAge = actualAge / twitterYears;
-  const roundedAge = Math.round(twitterAge);
 
-  const behavior = getBehaviorDescription(roundedAge);
-  const behaviorText = behavior.description;
-  const imageSource = behavior.imageSrc;
+  const roundedAge = Math.round(twitterAge); // Round the calculated age
+  const behaviorInfo = getBehaviorDescription(roundedAge); // Use rounded age for behavior description
+
+  const behaviorText = behaviorInfo.behavior;
+  const imageSource = behaviorInfo.imageSrc;
 
   document.getElementById('result').innerText = `Congratulations! Your Twitter age is approximately ${roundedAge} years old. ${behaviorText}`;
 
   const ageImage = document.getElementById('ageImage');
-  ageImage.src = imageSource;
-  ageImage.style.display = 'block';
-  ageImage.style.maxWidth = '80%';
-}
-
-// Function to determine behavior description based on age
-function getBehaviorDescription(twitterAge) {
-  let behavior = '';
-  let imageSrc = '';
-
-  if (twitterAge <= 5) {
-    behavior = "You're throwing Twitter tantrums like a toddler denied their favorite toy!";
-    imageSrc = 'images/toddler.jpg';
-  } else if (twitterAge > 5 && twitterAge <= 10) {
-    behavior = "You're the petulant prince/princess of Twitter, always wanting attention and retweets!";
-    imageSrc = 'images/kid.jpg';
-  } else if (twitterAge > 10 && twitterAge <= 15) {
-    behavior = "Your tweets are a whirlwind of teenage rebellion and immaturity!";
-    imageSrc = 'images/teenager.jpg';
-  } else if (twitterAge > 15 && twitterAge <= 30) {
-    behavior = "Your Twitter antics are the epitome of youthful exuberance and occasional immaturity!";
-    imageSrc = 'images/young.jpg';
-  } else if (twitterAge > 30 && twitterAge <= 50) {
-    behavior = "Your Twitter presence combines wisdom with occasional 'back in my day' rants!";
-    imageSrc = 'images/mature.jpg';
+  if (imageSource) {
+    ageImage.style.display = 'block';
+    ageImage.src = imageSource;
   } else {
-    behavior = "You're the sage of Twitter, occasionally throwing shade like a well-aged fine wine.";
-    imageSrc = 'images/wise.jpg';
+    ageImage.style.display = 'none';
   }
 
-  return {
-    description: behavior,
-    imageSrc: imageSrc
-  };
+  // Hide the title image once the result is displayed
+  document.getElementById('titleImage').style.display = 'none';
 }
-
-// Function to handle Enter key press
-function handleKeyPress(event) {
-  if (event.key === 'Enter') {
-    calculateTwitterAge(); // Call the calculateTwitterAge function on Enter key press
-  }
-}
-
-// Event listener for key press
-document.addEventListener('keypress', handleKeyPress);
