24b46540cc9e8a0ed9164e4c50f6f1d826da53e5
lamentierschweinchen <64620972+lamentierschweinchen@users.noreply.github.com>
2023-12-30 13:46:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,38 @@ function getBehaviorDescription(twitterAge) {
   let behavior = '';
   let imageSrc = '';
 
-  const roundedAge = Math.round(twitterAge); // Round the calculated age
+  const roundedAge = Math.round(twitterAge);
 
   if (roundedAge <= 1) {
     behavior = "You're tweeting like a crying baby, throwing tantrums with each tweet!";
-    imageSrc = 'images/crying_baby.jpg'; // Update path to crying baby image
+    imageSrc = 'images/crying_baby.jpg';
   } else if (roundedAge > 1 && roundedAge <= 3) {
     behavior = "Your tweets are nothing more than a chaotic string of gibberish, much like a toddler's nonsensical babbling!";
-    imageSrc = 'images/toddler.jpg'; // Update path to toddler image
+    imageSrc = 'images/toddler.jpg';
   } else if (roundedAge > 3 && roundedAge <= 5) {
     behavior = "Your Twitter presence is as chaotic and messy as a preschooler's playroom!";
-    imageSrc = 'images/mischievous_preschooler.jpg'; // Update path to mischievous preschooler image
+    imageSrc = 'images/mischievous_preschooler.jpg';
   } else if (roundedAge > 5 && roundedAge <= 7) {
     behavior = "You're tweeting like a whiny grade-schooler, complaining about everything!";
-    imageSrc = 'images/kid.jpg'; // Update path to kid image
+    imageSrc = 'images/kid.jpg';
   } else if (roundedAge > 7 && roundedAge <= 10) {
     behavior = "Your tweets are as irritating and rebellious as a stubborn pre-teen!";
-    imageSrc = 'images/stubborn_pre_teen.jpg'; // Update path to stubborn pre-teen image
+    imageSrc = 'images/stubborn_pre_teen.jpg';
   } else if (roundedAge > 10 && roundedAge <= 15) {
     behavior = "Your tweets are a whirlwind of teenage rebellion and immaturity!";
-    imageSrc = 'images/teenager.jpg'; // Update path to teenager image
+    imageSrc = 'images/teenager.jpg';
   } else if (roundedAge > 15 && roundedAge <= 18) {
     behavior = "You're a walking contradiction on Twitter - part adult, part teenager, and all drama!";
-    imageSrc = 'images/torn.jpg'; // Update path to young age image
+    imageSrc = 'images/torn.jpg';
   } else if (roundedAge > 18 && roundedAge <= 30) {
     behavior = "Your Twitter antics are the epitome of youthful exuberance and occasional immaturity!";
-    imageSrc = 'images/young.jpg'; // Update path to mature age image
+    imageSrc = 'images/young.jpg';
   } else if (roundedAge > 30 && roundedAge <= 45) {
     behavior = "Your Twitter posting is more focused on clever quips and teaching the youngens than little hissy fits!";
-    imageSrc = 'images/mature.jpg'; // Update path to mature age image
+    imageSrc = 'images/mature.jpg';
   } else {
     behavior = "You're the sage of Twitter, occasionally throwing shade like a well-aged fine wine.";
-    imageSrc = 'images/wise.jpg'; // Update path to wise age image
+    imageSrc = 'images/wise.jpg';
   }
 
   const result = {
@@ -44,8 +44,12 @@ function getBehaviorDescription(twitterAge) {
   return result;
 }
 
+function showCalculator() {
+  document.querySelector('.front-page').style.display = 'flex';
+  document.querySelector('.result-page').style.display = 'none';
+}
 
-function calculateTwitterAge() {
+function showResult() {
   const actualAge = parseInt(document.getElementById('actualAge').value);
   const twitterYears = parseInt(document.getElementById('twitterYears').value);
 
@@ -55,9 +59,8 @@ function calculateTwitterAge() {
   }
 
   const twitterAge = actualAge / twitterYears;
-
-  const roundedAge = Math.round(twitterAge); // Round the calculated age
-  const behaviorInfo = getBehaviorDescription(roundedAge); // Use rounded age for behavior description
+  const roundedAge = Math.round(twitterAge);
+  const behaviorInfo = getBehaviorDescription(roundedAge);
 
   const behaviorText = behaviorInfo.behavior;
   const imageSource = behaviorInfo.imageSrc;
@@ -72,6 +75,8 @@ function calculateTwitterAge() {
     ageImage.style.display = 'none';
   }
 
-  // Hide the title image once the result is displayed
-  document.getElementById('titleImage').style.display = 'none';
+  document.querySelector('.front-page').style.display = 'none';
+  document.querySelector('.result-page').style.display = 'block';
 }
+
+document.getElementById('tryAgainBtn').addEventListener('click', showCalculator);
