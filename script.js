c00f3153ed49a5f19afdf306ef0377ec9ac48ed8
lamentierschweinchen <64620972+lamentierschweinchen@users.noreply.github.com>
2023-12-30 15:29:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getBehaviorDescription(twitterAge) {
   let behavior = '';
   let imageSrc = '';
-  const roundedAge = Math.round(twitterAge);
+
+  const roundedAge = Math.round(twitterAge); // Round the calculated age
 
   if (roundedAge <= 1) {
     behavior = "You're tweeting like a crying baby, throwing tantrums with each tweet!";
@@ -34,7 +35,6 @@ function getBehaviorDescription(twitterAge) {
     behavior = "You're the sage of Twitter, occasionally throwing shade like a well-aged fine wine.";
     imageSrc = 'images/wise.jpg';
   }
-
   const result = {
     behavior: behavior,
     imageSrc: imageSrc
@@ -42,46 +42,34 @@ function getBehaviorDescription(twitterAge) {
 
   return result;
 }
-function showResult() {
+
+function calculateTwitterAge() {
   const actualAge = parseInt(document.getElementById('actualAge').value);
   const twitterYears = parseInt(document.getElementById('twitterYears').value);
 
   if (isNaN(actualAge) || isNaN(twitterYears) || twitterYears === 0) {
-    document.getElementById('resultText').innerText = 'Please enter valid numbers.';
+    document.getElementById('result').innerText = 'Please enter valid numbers.';
     return;
   }
 
   const twitterAge = actualAge / twitterYears;
-  const roundedAge = Math.round(twitterAge);
-  const behaviorInfo = getBehaviorDescription(roundedAge);
+
+  const roundedAge = Math.round(twitterAge); // Round the calculated age
+  const behaviorInfo = getBehaviorDescription(roundedAge); // Use rounded age for behavior description
 
   const behaviorText = behaviorInfo.behavior;
   const imageSource = behaviorInfo.imageSrc;
 
-  document.getElementById('resultText').innerText = behaviorText;
+  document.getElementById('result').innerText = `Congratulations! Your Twitter age is approximately ${roundedAge} years old. ${behaviorText}`;
 
   const ageImage = document.getElementById('ageImage');
   if (imageSource) {
-    ageImage.src = imageSource;
     ageImage.style.display = 'block';
+    ageImage.src = imageSource;
   } else {
     ageImage.style.display = 'none';
   }
 
-  document.querySelector('.front-page').style.display = 'none';
-  document.querySelector('.result-page').style.display = 'block';
+  // Hide the title image once the result is displayed
+  document.getElementById('titleImage').style.display = 'none';
 }
-
-function resetCalculator() {
-  document.getElementById('resultText').innerText = '';
-  document.getElementById('ageImage').style.display = 'none';
-  document.querySelector('.result-page').style.display = 'none';
-  document.querySelector('.front-page').style.display = 'flex';
-  document.getElementById('actualAge').value = '';
-  document.getElementById('twitterYears').value = '';
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('calculateBtn').addEventListener('click', showResult);
-  document.getElementById('tryAgainBtn').addEventListener('click', resetCalculator);
-});
